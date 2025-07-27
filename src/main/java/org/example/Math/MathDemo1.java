4993292fb2613db04039102fb8cc6482e96daa03
Toby Armstrong <513578967@qq.com>
2025-07-27 21:52:52 +0800

    Math

---
+++ src/main/java/org/example/Math/MathDemo1.java
@@ -0,0 +1,9 @@
+package org.example.Math;
+
+public class MathDemo1 {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-2147483648));
+        System.out.println(Math.abs(-2147483647));
+        System.out.println(Math.abs(20));
+    }
+}
