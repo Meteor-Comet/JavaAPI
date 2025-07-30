17a2fe260d1cb2a0f380019845ba532e5416d08e
Toby Armstrong <513578967@qq.com>
2025-07-30 11:29:43 +0800

    regex

--- src/main/java/org/example/regex/RegexDemo1.java
+++ src/main/java/org/example/regex/RegexDemo1.java
@@ -1,9 +1,20 @@
 package org.example.regex;
 
+import java.util.Scanner;
+
 public class RegexDemo1 {
     public static void main(String[] args) {
-        String qq = "123456789";
-        boolean b = qq.matches("[1-9]\\d{5,10}");
-        System.out.println(b);
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入手机号码:");
+        String phoneNumber = in.next();
+        String regex1 = "^1[3-9]\\d{9}$";
+        boolean matches1 = phoneNumber.matches(regex1);
+        System.out.println(matches1);
+
+        System.out.println("请输入邮箱:");
+        String email = in.next();
+        String regex2 = "\\w+@[\\w&&[^_]]{2,6}(\\.[a-zA-Z]{2,3}){1,2}";
+        boolean matches2 = email.matches(regex2);
+        System.out.println(matches2);
     }
 }
